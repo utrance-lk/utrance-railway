a4cbbd14e002244c8254ab2c0531cb883f2d4560
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2020-10-11 18:39:55 +0530

    render view finalized

---
+++ views/pages/home/home.php
@@ -0,0 +1,28 @@
+<div class="content">
+        <div class="primary-content">
+          <div class="primary-content--intro">intro</div>
+          <div class="primary-content--searchbar">
+            <div class="search-box">
+              <div class="search-box--inside">
+                <form action="" method="POST" class="search-bar__form">
+                  <div class="search-bar__form-1">
+                    <label for="from">From</label>
+                    <input type="text" id="from" name="from" />
+                  </div>
+                  <div class="search-bar__form-2">
+                    <label for="to">To</label>
+                    <input type="text" id="to" name="to" />
+                  </div>
+                  <div class="search-bar__form-3">
+                    <label for="when">When</label>
+                    <input type="date" id="when" name="when" />
+                  </div>
+                  <div class="search-btn">
+                    <button type="submit" class="btn">search <i class="fas fa-search"></i></button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
