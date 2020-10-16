d4801778f3fbd8971bbbc5d92b1c59faecda8e26
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2020-10-16 08:57:41 +0530

    direct path searching added

--- models/ViewModel.php
+++ models/ViewModel.php
@@ -23,25 +23,54 @@ class ViewModel extends Model{
         $fromSearchId = $fromSearchId->fetchAll(PDO::FETCH_ASSOC);
         $toSearchId = $toSearchId->fetchAll(PDO::FETCH_ASSOC);
 
-        $fromId;
-        $toId;
+        
+        if($fromSearchId && $toSearchId) {
 
-        foreach($fromSearchId as $row) {
-            $fromId = $row['station_id'];
+            $fromId;
+            $toId;
+
+            foreach ($fromSearchId as $row) {
+                $fromId = $row['station_id'];
+            }
+
+            foreach ($toSearchId as $row) {
+                $toId = $row['station_id'];
+            }
+            
+            // 1) search for a direct path
+            $this->searchDirectPath($fromId, $toId);
+            
+        } else {
+            echo 'station not found!';
+            return 'station not found!!';
         }
-        foreach($toSearchId as $row) {
-            $toId = $row['station_id'];
-        }
 
-        $fromSearchRouteId = APP::$APP->db->pdo->prepare("SELECT route_id FROM stops WHERE station_id=:from");
+    }
 
-        $fromSearchRouteId->bindValue(":from", $fromId);
-        $fromSearchRouteId->execute();
+    protected function searchDirectPath($fromId, $toId) {
+        $searchDirectPath = APP::$APP->db->pdo->prepare(
+                "SELECT * FROM
+                    (SELECT fromt.route_id AS route_id,
+                            fromt.station_id AS fssid, fromt.departure_time AS fssdt, fromt.path_id AS fsspi,
+                            tot.station_id AS tseid, tot.arrival_time AS tseat, tot.path_id AS tsepi
+                    FROM
+                        (SELECT route_id, path_id, departure_time, station_id FROM stops WHERE station_id =:from) fromt
+                    INNER JOIN
+                        (SELECT route_id, path_id, arrival_time, station_id FROM stops WHERE station_id =:to) tot
+                    ON fromt.route_id = tot.route_id) AS matching_table
+                WHERE fsspi <= tsepi
+            ");
 
-        $fromSearchRouteId = $fromSearchRouteId->fetchAll(PDO::FETCH_ASSOC);
+        $searchDirectPath->bindValue(":from", $fromId);
+        $searchDirectPath->bindValue(":to", $toId);
 
-        foreach($fromSearchRouteId as $row) {
+        $searchDirectPath->execute();
+
+        $searchDirectPath = $searchDirectPath->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($searchDirectPath as $row) {
             var_dump($row);
+        echo "</br>";
         }
 
     }
