d235dbca9d4019ab508a91e9099e5b9381061c35
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2020-10-27 17:41:08 +0530

    add files

--- public/index.php
+++ public/index.php
@@ -72,7 +72,7 @@ $app->router->post('/utrance-railway/t/test.php', [ViewController::class, 'searc
 
 
 
-$app->router->get('/utrance-railway/cat', [AddTrainDetailsController::class, 'addTrain']);
+$app->router->post('/utrance-railway/addTrain', [AdminController::class, 'addTrain']);
 
 $app->router->get('/utrance-railway/public/index.php/admin', [AdminController::class, 'adminSettings']);
 
