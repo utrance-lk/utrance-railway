38d62e5c2eca41b92d9c910731610e7189fb2bd4
Daran <daranyasehar@gmail.com>
2020-10-31 21:53:43 +0530

    signInPageNow

--- models/UserModel.php
+++ models/UserModel.php
@@ -51,31 +51,47 @@ class UserModel extends Model
 public function signIn()
 {
       
-if(isset($_POST["login"])){
-  if(empty($_POST["id"])||empty($_POST["user_password"])){
-    $message = 'All fields are required';
-  }	
-  else{
-    $query = "SELECT * FROM users WHERE id = :id AND user_password = :user_password";
-    $statement = $connect->prepare($query);
-    $statement->execute(
-      array(
-        'id' => $_POST["id"],
-        'user_password' => $_POST["user_password"]
-      )
-    );
-    $count = $statement->rowCount();
-    if($count>0)
-    {
-      $_SESSION["id"]=$_POST["id"];
-      header("location:home.php");
-    }
-    else{
-      $message='Wrong Data';
-    }
+  session_start();
 
+  $host="localhost";
+  $database="utrance";
+  $username = "root";
+  $password = "";
+  $message = "";
+  
+  try{
+    $connect = new PDO("mysql:host=$host;dbname=$database", $username,$password);
+    $connect-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    if(isset($_POST["login"])){
+         if(empty($_POST["id"])||empty($_POST["user_password"])){
+             $message = 'All fields are required';
+         }	
+         else{
+             $query = "SELECT * FROM users WHERE id = :id AND user_password = :user_password";
+             $statement = $connect->prepare($query);
+             $statement->execute(
+                 array(
+                     'id' => $_POST["id"],
+                     'user_password' => $_POST["user_password"]
+                 )
+             );
+             $count = $statement->rowCount();
+             if($count>0)
+             {
+                 $_SESSION["id"]=$_POST["id"];
+                 header("location:home.php");
+             }
+             else{
+                 $message='Wrong Data';
+             }
+
+         }
+     }
   }
-}
+  catch(PDOException $error){
+      $message = $error->getMessage();
+  }
           
 }
   
