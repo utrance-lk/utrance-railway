63fe4668504383aef2e864b1ff2470e9f17b3b1c
Daran <daranyasehar@gmail.com>
2020-10-31 20:43:29 +0530

    signInPassword

--- models/UserModel.php
+++ models/UserModel.php
@@ -46,6 +46,38 @@ class UserModel extends Model
     $query->bindValue(":us", $this->user_role);
       return $query->execute();
     }
+
+public function signIn(){
+          
+	if(isset($_POST["login"])){
+		if(empty($_POST["username"])||empty($_POST["user_password"])){
+			$message = 'All fields are required';
+		}	
+		else{
+			$query = "SELECT * FROM users WHERE username = :username AND user_password = :user_password";
+			$statement = $connect->prepare($query);
+			$statement->execute(
+				array(
+					'username' => $_POST["username"],
+					'user_password' => $_POST["user_password"]
+				)
+			);
+			$count = $statement->rowCount();
+			if($count>0)
+			{
+				$_SESSION["username"]=$_POST["username"];
+				header("location:home.php");
+			}
+			else{
+				$message='<label>Wrong Data</label>';
+			}
+
+		}
+	}
+            
+            
+
+    }
     
    
     
