19a7f18890865dabc08c41575664bde2b34d688a
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2020-11-08 11:22:14 +0530

    changes in login

--- views/pages/login/main.php
+++ views/pages/login/main.php
@@ -9,10 +9,6 @@
       href="https://fonts.googleapis.com/css?family=Lato:300,300i,700"
     />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
-    <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
-    rel="stylesheet"
-    />
     <link rel="stylesheet" href="../../../../utrance-railway/public/css/base.css" />
     <link rel="stylesheet" href="../../../../utrance-railway/public/css/pages/login/login.css" />
   </head>
