699d24f679ea857510662498b85932658efbbadd
Hasani <65714939+kanthi97@users.noreply.github.com>
2020-11-17 19:52:42 +0530

    modified

--- views/pages/FreightServicePrice/main.php
+++ views/pages/FreightServicePrice/main.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Update Profile</title>
+    <title>Frieght Service</title>
     <link rel="stylesheet" type="text/css" href="../../../../utrance-railway/public/css/admin/update_userProfile.css" />
   <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
