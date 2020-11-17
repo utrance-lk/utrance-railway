2cca42558df068ca452d3f9f27bae808d84ba873
Hasani <65714939+kanthi97@users.noreply.github.com>
2020-11-17 19:50:15 +0530

    create function

---
+++ views/pages/FreightServicePrice/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Profile</title>
+    <link rel="stylesheet" type="text/css" href="../../../../utrance-railway/public/css/admin/update_userProfile.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../../utrance-railway/public/css/layout/footer.css" />
+    <link rel="stylesheet" href="../../../utrance-railway/public/css/ticketPrice/ticketPrice.css" />
+    
+    <link rel="stylesheet" href="../../../utrance-railway/public/css/base.css" />
+</head>
