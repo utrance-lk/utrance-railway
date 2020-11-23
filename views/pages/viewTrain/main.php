9d6f71f1526e177acf0f008113b6be18f05a8ebf
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2020-11-23 12:22:46 +0530

    changes in viewstrains

--- views/pages/viewTrain/main.php
+++ views/pages/viewTrain/main.php
@@ -19,7 +19,7 @@
     />
 
     <link rel="stylesheet" href="../../../utrance-railway/public/css/base.css" />
-    <link rel="stylesheet" href="../../../../utrance-railway/public/css/layout/header.css" />
+    <link rel="stylesheet" href="../../../../utrance-railway/public/css/layout/headerMain.css" />
     <link rel="stylesheet" href="../../../utrance-railway/public/css/pages/viewTrain/viewTrain.css" />
     <link rel="stylesheet" href="../../../../utrance-railway/public/css/layout/footer.css" />
 
