28c54aaa52c9980e6cb4f857d9d2bc8263f7740c
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-01-09 15:56:08 +0530

    newsfeed part refactored

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -1,55 +1,16 @@
-<section class="home__heading-primary">
-        <form class="searchbar-main" method="POST" action="/utrance-railway/search">
-          <!-- <div class="search-bar"> -->
-            <div class="searchbar-main__search">
-                <div class="searchbar-main__from-container">
-                  <div class="searchbar-main__from">
-                    <span>From</span>
-                    <div class="searchbar-main__station--from-container">
-                      <div class="searchbar-main__station--from js--from__station" id="js--from__station"></div>
-                      <div class="searchbar-main__swap-btn js--swap-btn">
-                        <svg class="searchbar-main__icon">
-                          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-swap"></use>
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="searchbar-main__dropdown js--search-dropdown__from">
-                    <input type="text" id="dropdown-from" name="from" class="searchbar-main__dropdown-input js--search-dropdown__search-from">
-                  </div>
-                  <ul class="searchbar-main__dropdown-search-results js--results__list-from"></ul>
-                </div>
-              <div class="searchbar-main__to-container">
-                <div class="searchbar-main__to">
-                  <span>To</span>
-                  <div class="searchbar-main__station--to js--to__station" id="js--to__station"></div>
-                </div>
-                <div class="searchbar-main__dropdown js--search-dropdown__to">
-                    <input type="text" id="dropdown-to" name="to" class="searchbar-main__dropdown-input js--search-dropdown__search-to">
-                </div>
-                <ul class="searchbar-main__dropdown-search-results js--results__list-to"></ul>
-              </div>
-              <div>
-                <div class="searchbar-main__when">
-                  <span>When</span>
-                  <input type="date" class="searchbar-main__station--when js--when__date" name="when" />
-                </div>
-              </div>
-            </div>
-            <input type="submit" value="Search" class="searchbar-main__search-btn js--searchbar__search-btn">
-          <!-- </div> -->
-        </form>
-      </section>
+  <section class="home__heading-primary">
+    <?php include_once "../views/components/searchbarMain.php"?>
+  </section>
 
-      <?php include 'newsfeed.php'; ?>
+  <?php include_once 'newsfeed.php';?>
 
-      <script type="text/javascript" src="../../../utrance-railway/public/js/components/mainSearch.js"></script>
-      <script type="text/javascript" src="../../../utrance-railway/public/js/components/currentDate.js"></script>
-      <script>
-        document.querySelector(".js--from__station").textContent = stationsArray[3];
-        document.querySelector(".js--to__station").textContent = stationsArray[0];
-        document.querySelector(".js--search-dropdown__search-from").value = stationsArray[3];
-        document.querySelector(".js--search-dropdown__search-to").value = stationsArray[0];
-      </script>
+  <script type="text/javascript" src="../../../utrance-railway/public/js/components/mainSearch.js"></script>
+  <script type="text/javascript" src="../../../utrance-railway/public/js/components/currentDate.js"></script>
+  <script>
+    document.querySelector(".js--from__station").textContent = stationsArray[3];
+    document.querySelector(".js--to__station").textContent = stationsArray[0];
+    document.querySelector(".js--search-dropdown__search-from").value = stationsArray[3];
+    document.querySelector(".js--search-dropdown__search-to").value = stationsArray[0];
+  </script>
   </body>
 </html>
--- views/pages/home/main.php
+++ views/pages/home/main.php
@@ -22,7 +22,6 @@
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
     />
 
-    <link rel="stylesheet" href="../../../utrance-railway/public/css/pages/home/home.css" />
     <link rel="stylesheet" href="../../../utrance-railway/public/css/pages/home/newsfeed.css" />
     <!-- <link rel="stylesheet" href="../../../../utrance-railway/public/css/layout/footer.css" /> -->
 
