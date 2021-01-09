4597024d0e99b1e1666d97843e9cbb74dab79012
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-01-09 11:31:34 +0530

    searchbar refactored

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -1,43 +1,43 @@
-<section class="heading-primary">
-        <form class="search-bar__container" method="POST" action="/utrance-railway/search">
-          <div class="search-bar">
-            <div class="search-bar__search">
-                <div class="search-bar__from-container">
-                  <div class="searchbar__from">
+<section class="home__heading-primary">
+        <form class="searchbar-main" method="POST" action="/utrance-railway/search">
+          <!-- <div class="search-bar"> -->
+            <div class="searchbar-main__search">
+                <div class="searchbar-main__from-container">
+                  <div class="searchbar-main__from">
                     <span>From</span>
-                    <div class="from__station-container">
-                      <div class="from__station js--from__station" id="js--from__station"></div>
-                      <div class="swap-button js--swap-btn">
-                        <svg class="swap-icon">
+                    <div class="searchbar-main__station--from-container">
+                      <div class="searchbar-main__station--from js--from__station" id="js--from__station"></div>
+                      <div class="searchbar-main__swap-btn js--swap-btn">
+                        <svg class="searchbar-main__icon">
                           <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-swap"></use>
                         </svg>
                       </div>
                     </div>
                   </div>
-                  <div class="search-dropdown search-dropdown__from js--search-dropdown__from">
-                    <input type="text" id="dropdown-from" name="from" class="search-dropdown__search js--search-dropdown__search-from">
+                  <div class="searchbar-main__dropdown js--search-dropdown__from">
+                    <input type="text" id="dropdown-from" name="from" class="searchbar-main__dropdown-input js--search-dropdown__search-from">
                   </div>
-                  <ul class="search-dropdown__search-results js--results__list-from"></ul>
+                  <ul class="searchbar-main__dropdown-search-results js--results__list-from"></ul>
                 </div>
-              <div class="search-bar__to-container">
-                <div class="searchbar__to">
+              <div class="searchbar-main__to-container">
+                <div class="searchbar-main__to">
                   <span>To</span>
-                  <div class="to__station js--to__station" id="js--to__station"></div>
+                  <div class="searchbar-main__station--to js--to__station" id="js--to__station"></div>
                 </div>
-                <div class="search-dropdown search-dropdown__to js--search-dropdown__to">
-                    <input type="text" id="dropdown-to" name="to" class="search-dropdown__search js--search-dropdown__search-to">
+                <div class="searchbar-main__dropdown js--search-dropdown__to">
+                    <input type="text" id="dropdown-to" name="to" class="searchbar-main__dropdown-input js--search-dropdown__search-to">
                 </div>
-                <ul class="search-dropdown__search-results js--results__list-to"></ul>
+                <ul class="searchbar-main__dropdown-search-results js--results__list-to"></ul>
               </div>
-              <div class="search-bar__when-container">
-                <div class="searchbar__when">
+              <div>
+                <div class="searchbar-main__when">
                   <span>When</span>
-                  <input type="date" class="when__date js--when__date" name="when" />
+                  <input type="date" class="searchbar-main__station--when js--when__date" name="when" />
                 </div>
               </div>
             </div>
-            <input type="submit" value="Search" class="search-bar__search-btn js--searchbar__search-btn">
-          </div>
+            <input type="submit" value="Search" class="searchbar-main__search-btn js--searchbar__search-btn">
+          <!-- </div> -->
         </form>
       </section>
 
