2687a40b89edc5cca45dfa135b25489e639ff6be
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-03-21 01:28:58 +0530

    another refatoring

--- views/layouts/adminSideNav.php
+++ views/layouts/adminSideNav.php
@@ -2,13 +2,13 @@
     <div class="sidebar-nav__common">
       <a href="/utrance-railway/settings" class="margin-t-m sidebar-nav__item js--sidebar__nav-settings">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-cog"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-cog"></use>
         </svg>
         <span>Settings</span>
       </a>
       <a href="/utrance-railway/myBookings" class="sidebar-nav__item">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-suitcase"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-suitcase"></use>
         </svg>
         <span>My bookings</span>
       </a>
@@ -17,37 +17,37 @@
     <div class="margin-b-l">
       <a href="/utrance-railway/trains" class="sidebar-nav__item margin-t-m">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-paper-plane"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-paper-plane"></use>
         </svg>
         <span>Manage trains</span>
       </a>
       <a href="/utrance-railway/routes/" class="sidebar-nav__item">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-location"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-location"></use>
         </svg>
         <span>Manage routes</span>
       </a>
       <a href="/utrance-railway/users" class="sidebar-nav__item js--sidebar__nav-manage--users">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-man"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-man"></use>
         </svg>
         <span>Manage users</span>
       </a>
       <a href="/utrance-railway/bookings" class="sidebar-nav__item">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-map"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-map"></use>
         </svg>
         <span>Manage bookings</span>
       </a>
       <a href="/utrance-railway/freight-bookings" class="sidebar-nav__item">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-shopping-cart"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-shopping-cart"></use>
         </svg>
         <span>Manage freights</span>
       </a>
       <a href="/utrance-railway/manage-news" class="sidebar-nav__item">
         <svg class="sidebar-nav__icon">
-          <use xlink:href="../../../../utrance-railway/public/img/svg/sprite.svg#icon-landline"></use>
+          <use xlink:href="/public/img/svg/sprite.svg#icon-landline"></use>
         </svg>
         <span>Manage News</span>
       </a>
