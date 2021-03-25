0ea8f9c39bd8fe8ceb203943faaf6194cc670ce7
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-03-25 07:17:39 +0530

    payment issues fixed

--- routes/paymentRoutes.php
+++ routes/paymentRoutes.php
@@ -3,8 +3,6 @@
 
 require_once "../controllers/PaymentController.php";
 
-
-
 // payment
 $app->router->post('/utrance-railway/payment', [PaymentController::class, 'payment']);
 $app->router->get('/utrance-railway/payment', [PaymentController::class, 'payment']);
