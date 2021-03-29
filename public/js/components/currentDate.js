372e817a2a8249753df392a223481a32669a385e
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-03-29 07:31:04 +0530

    current date issue fixed

--- public/js/components/currentDate.js
+++ public/js/components/currentDate.js
@@ -4,6 +4,8 @@ let setDate = function () {
   let mm = String(today.getMonth() + 1);
   let yyyy = today.getFullYear();
   today = yyyy + "-" + formatDate(mm) + "-" + formatDate(dd);
+  var maxDay = yyyy + "-" + formatDate(mm*1 + 5) + "-" + formatDate(dd);
+  items.inputDate.max = maxDay;
 
   items.inputDate.value = today;
   items.inputDate.min = today;
@@ -16,9 +18,19 @@ let formatDate = function (date) {
   return date;
 };
 
-
 window.addEventListener("load", function () {
-  setDate();
+  if (!items.inputDate.value) {
+    setDate();
+  } else {
+    let today = new Date();
+    let dd = String(today.getDate());
+    let mm = String(today.getMonth() + 1);
+    let yyyy = today.getFullYear();
+    today = yyyy + "-" + formatDate(mm) + "-" + formatDate(dd);
+    var maxDay = yyyy + "-" + formatDate(mm*1 + 5) + "-" + formatDate(dd);
+    items.inputDate.min = today;
+    items.inputDate.max = maxDay;
+  }
   // // items.fromStationLabel.textContent = inputSearchFrom.value;
   // console.log(items.fromStationLabel.textContent);
 });
