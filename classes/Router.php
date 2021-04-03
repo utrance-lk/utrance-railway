5573b2e00ce2ba909811da97fd6d7bd2adf08b16
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2021-04-03 17:55:36 +0530

    main.php removed from view pages

--- classes/Router.php
+++ classes/Router.php
@@ -40,7 +40,7 @@
                 return $this->renderView($callback);
             }
 
-            if(is_array($callback)) {
+            if (is_array($callback)) {
                 $callback[0] = new $callback[0];
             }
 
@@ -53,31 +53,17 @@
             $base = $this->loadBase();
             $footer = $this->layoutContent('footer');
             $header = $this->layoutContent('header');
-            
-            $main = $this->renderOnlyView($view, $params, true);
+
             $viewContent = $this->renderOnlyView($view, $params);
 
-            $base = str_replace('{{main}}', $main, $base);
             $base = str_replace('{{header}}', $header, $base);
-            // if(App::$APP->activeUser()['role'] === 'admin') {
-            //     $adminSideNav = $this->layoutContent('adminSideNav');
-            //     $base = str_replace('{{adminSideNav}}', $adminSideNav, $base);
-            // }
-            // if(App::$APP->activeUser()['role'] === 'user') {
-            //     $userSideNav = $this->layoutContent('userSideNav');
-            //     $base = str_replace('{{userSideNav}}', $userSideNav, $base);
-            // }
-        // if(App::$APP->activeUser()['role'] === 'detailsProvider') {
-        //         $detailsProviderSideNav = $this->layoutContent('detailsProviderSideNav');
-        //         $base = str_replace('{{detailsProviderSideNav}}', $detailsProviderSideNav, $base);
-        //     }
             $base = str_replace('{{content}}', $viewContent, $base);
 
             return str_replace('{{footer}}', $footer, $base);
-
         }
 
-        protected function loadBase() {
+        protected function loadBase()
+        {
             ob_start();
             include_once "../views/base.php";
             return ob_get_clean();
@@ -90,29 +76,20 @@
             return ob_get_clean();
         }
 
-        protected function renderOnlyView($view, $params, $isMain = false)
+        protected function renderOnlyView($view, $params)
         {
-            if($isMain) {
-                ob_start();
-                if(is_array($view)) {
-                    $view = $view[0];
-                }
-                include_once "../views/pages/$view/main.php";
-                return ob_get_clean();
-            } else {  
-                foreach($params as $key => $value) {
-                    $$key = $value;
-                }
-                ob_start();
-                if(is_array($view)) {
-                    
-                    $view = implode('/', $view);
-                    include_once "../views/pages/$view.php";
-                } else {
-                    include_once "../views/pages/$view/$view.php";
-                }
-                return ob_get_clean();
+            foreach ($params as $key => $value) {
+                $$key = $value;
             }
+            ob_start();
+            if (is_array($view)) {
+
+                $view = implode('/', $view);
+                include_once "../views/pages/$view.php";
+            } else {
+                include_once "../views/pages/$view/$view.php";
+            }
+            return ob_get_clean();
         }
 
     }
